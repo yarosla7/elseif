a6eab73fe4a096e3ecb463e726758fbd0e36c755
y.yanin <yanin.yarosla7@gmail.com>
2022-12-06 20:09:26 +0300

    homework is done.

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,7 @@ public class Main {
         System.out.println("\nЗадача_1:");
 
         int clientOS = 0;
+
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке.");
         } if (clientOS == 1) {
@@ -41,18 +42,25 @@ public class Main {
 
         System.out.println("\nЗадача_2:");
 
-        int clientOS = 1;
-        int clientDeviceYear = 2009;
-        if (clientOS == 0 && clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
-        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+        int clientOS = 0;
+
+        int clientDeviceYear = 2020;
+
+        if (clientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+            } else {
                 System.out.println("Установите версию приложения для iOS по ссылке.");
-        } else if (clientOS == 1 && clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
-        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
-                System.out.println("Установите версию приложения для Android по ссылке.");
             }
-    }
+            } else {
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+                } else {
+                    System.out.println("Установите версию приложения для Android по ссылке.");
+
+                }
+            }
+        }
 
     public static void task3() {
 
@@ -67,7 +75,9 @@ public class Main {
         System.out.println("\nЗадача_3:");
 
         int year = 2004;
+
         boolean leapYear = year % 400 == 0 || year % 4 == 0 && year % 100 !=0;
+
         if (leapYear) {
             System.out.println(year + " год является високосным.");
         } else {
@@ -91,7 +101,9 @@ public class Main {
         System.out.println("\nЗадача_4:");
 
         int deliveryDistance = 44;
+
         int daysDelivery;
+
         if (deliveryDistance <= 20) {
             daysDelivery = 1;
             System.out.println("Потребуются дней: " + daysDelivery + " сутки");
